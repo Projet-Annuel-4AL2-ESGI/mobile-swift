59faff6adbc1f51307c1d28c83719204851c98d0
halimatou-cpu <halimatoubah.bh@gmail.com>
2022-10-23 17:34:37 +0200

    Initial Commit

---
+++ Codogo/CodogoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodogoApp.swift
+//  Codogo
+//
+//  Created by Halimatou BAH on 23/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct CodogoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
